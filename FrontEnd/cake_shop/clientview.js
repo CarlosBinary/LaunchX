b0b20c36b4ec20fec5d8ab0c75cb3bc4b55d7a6f
CarlosBinary <lx16575@innovaccion.mx>
2022-11-24 17:47:45 -0600

    eliminando archivos viejos

--- FrontEnd/cake_shop/clientview.js
+++
@@ -1,7 +0,0 @@
-function hideDescription(flavor) {
-    let description = flavor.nextElementSibling;
-    let visibility = "visible";
-    if(description.style.visibility == visibility) visibility = "collapse";
-
-    description.style.visibility = visibility;
-}
